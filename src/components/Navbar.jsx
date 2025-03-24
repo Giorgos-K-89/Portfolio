8e658dcb4b78a5929b7e98d813f4d6d03c811a3c
Giorgos Konstas <gio.konstas@hotmial.com>
2025-03-24 17:15:52 +0200

    meta tags added and preconnect with google fonts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,11 @@ export default function Navbar() {
   return (
     <div className="absolute top-0 left-0 w-full px-5 md:px-12 py-6 flex flex-col gap-6 md:flex-row  items-center justify-between z-40 text-white">
       <div id="logo" className="flex items-center gap-2">
-        <img className="w-8 md:w-10 pointer-events-none" src="/logo.png" />
+        <img
+          className="w-8 md:w-10 pointer-events-none"
+          src="/logo.png"
+          alt="Giorgos Konstas' logo"
+        />
         <span className="text-lg sm:text-xl md:text-2xl tracking-wider">
           GIORGOS KONSTAS
         </span>
--- src/components/RotatingCircles.jsx
+++ src/components/RotatingCircles.jsx
@@ -4,27 +4,27 @@ export default function RotatingCircles() {
       {/* Outer Circle */}
       <div className="circle outer-circle">
         <div className="icon react">
-          <img src="/icons/react.png" />
+          <img src="/icons/react.png" alt="React logo" />
         </div>
         <div className="icon git">
-          <img src="/icons/git.png" />
+          <img src="/icons/git.png" alt="Git logo" />
         </div>
         <div className="icon postgres">
-          <img src="/icons/sql.png" />
+          <img src="/icons/sql.png" alt="PostrgreSQL logo" />
         </div>
       </div>
 
       {/* Inner Circle */}
       <div className="circle inner-circle">
         <div className="icon html">
-          <img src="/icons/html.png" />
+          <img src="/icons/html.png" alt="HTML logo" />
         </div>
-        <div className="icon css">
+        <div className="icon css" alt="CSS logo">
           {" "}
           <img src="/icons/css.png" />
         </div>
         <div className="icon js">
-          <img src="/icons/js.png" />
+          <img src="/icons/js.png" alt="Javascript logo" />
         </div>
       </div>
     </div>
