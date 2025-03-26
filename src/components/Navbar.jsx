b5563e4dd1d89d6ba8eb430e1c5ad9b84857e39e
Giorgos Konstas <gio.konstas@hotmial.com>
2025-03-26 12:28:01 +0200

    favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <div className="absolute top-0 left-0 w-full px-5 md:px-12 py-6 flex flex-col gap-6 md:flex-row  items-center justify-between z-40 text-white">
-      <div id="logo" className="flex items-center gap-2">
+      <Link id="logo" className="flex items-center gap-2" to="/">
         <img
           className="w-8 md:w-10 pointer-events-none"
           src="/logo.png"
@@ -10,7 +12,7 @@ export default function Navbar() {
         <span className="text-lg sm:text-xl md:text-2xl tracking-wider">
           GIORGOS KONSTAS
         </span>
-      </div>
+      </Link>
       <div className="flex gap-2 text-sm sm:text-base">
         <div className="flex gap-4 items-center">
           <span>Status :</span>
